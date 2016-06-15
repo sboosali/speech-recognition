d6ba26d3186acc864b0611162697e09635a7caa7
Spiros Boosalis <samboosalis@gmail.com>
2016-06-15 17:16:27 +0300

    pipes-http

--- sources/Recognition/Example.hs
+++ sources/Recognition/Example.hs
@@ -1,13 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-} -- to test inference
+
 module Recognition.Example where
 import Recognition()
+
+import Pipes
+import Pipes.HTTP
+import qualified Pipes.ByteString as PB
 
 {-|
 @
 stack build && stack exec -- example-speech-recognition
 @
--}
-main :: IO ()
-main = do
- putStrLn "(Recognition.Example...)"
 
+https://hackage.haskell.org/package/pipes-http-1.0.2/docs/Pipes-HTTP.html
+
+https://hackage.haskell.org/package/http-client-0.4.28/docs/Network-HTTP-Client.html
+
+
+-}
+main = do
+    putStrLn "(Recognition.Example...)"
+
+    _req <- parseUrl "https://www.example.com"
+    let req = post _req
+
+    m <- newManager tlsManagerSettings
+    withHTTP req m $ \resp ->
+            runEffect $ responseBody resp >-> PB.stdout
+
+post r = r
+ { method = "POST"
+ , requestBody = stream PB.stdin
+ }
